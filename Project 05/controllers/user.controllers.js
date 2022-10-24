7c5726cada11d64616e6dc5d74d89309e3d2657a
20120230 <duytruong5437@gmail.com>
2022-10-24 21:10:26 +0700

    mã hóa password

--- Project 05/controllers/user.controllers.js
+++ Project 05/controllers/user.controllers.js
@@ -1,10 +1,13 @@
 const { Op } = require("sequelize")
-const { User } = require("../models")
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
 const createUser = async (req, res) => {
     const {name, email, password, numberPhone} = req.body;
     try {
-        const newUser= await User.create({name, email, password, numberPhone, type: "client"});
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = bcrypt.hashSync(password, salt);
+        const newUser= await User.create({name, email, password: hashPassword, numberPhone, type: "client"});
         res.status(201).send(newUser);
     } catch (error) {
        res.status(500).send(error)
