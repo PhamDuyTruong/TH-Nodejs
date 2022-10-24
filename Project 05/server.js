741874917e29a3545b3496b914facbde5861400c
20120230 <duytruong5437@gmail.com>
2022-10-24 15:01:41 +0700

    add project

---
+++ Project 05/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const {sequelize}= require('./models');
+
+const {rootRouter} = require("./routers")
+
+
+const app = express();
+
+app.use(express.json());
+
+const publicPath = path.join(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.use('/api/v1', rootRouter);
+
+app.listen(8080, async () => {
+    console.log(`App running at localhost at ${8080}`);
+    try{
+       await sequelize.authenticate();
+       console.log('Connection successfully !!!!');
+    }catch(error){
+        console.log('Unable to connect')
+    }
+    
+    
+})
