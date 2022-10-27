f7997b298b0a25da716bf1be484b948b2b791f68
20120230 <duytruong5437@gmail.com>
2022-10-27 21:07:36 +0700

    authorize user

--- Project 05/routers/station.routers.js
+++ Project 05/routers/station.routers.js
@@ -1,12 +1,13 @@
 const {Station} = require("../models")
 const {checkExist} = require("../middleware/Validations/checkExist");
-const {authenticate} = require("../middleware/auth/authenticate")
+const {authenticate} = require("../middleware/auth/authenticate");
+const {authorize} = require("../middleware/auth/authorize")
 const express = require("express");
 const {createStation, getAllStation, getDetailStation, updateStation, deleteStation} = require("../controllers/station.controllers")
 
 const stationRouter = express.Router();
 
-stationRouter.post("/", authenticate, createStation);
+stationRouter.post("/", authenticate, authorize(["ADMIN"]), createStation);
 stationRouter.get("/", getAllStation);
 stationRouter.get("/:id", getDetailStation);
 stationRouter.put("/:id", checkExist(Station), updateStation);
