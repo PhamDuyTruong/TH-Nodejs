456442c33f63799f37bbe5156a9031ca73eaa90b
20120230 <duytruong5437@gmail.com>
2022-10-28 11:45:36 +0700

    fix upload

--- Project 05/routers/user.routers.js
+++ Project 05/routers/user.routers.js
@@ -17,7 +17,19 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + "_" +  file.originalname) // dặt lại tên cho file
     }
 })
-const upload = multer({storage: storage })
+const upload = multer({
+    storage: storage,
+    fileFilter: function(req, file, cb){
+        const extensionImageList = [".png", ".jpg"];
+        const extension = file.originalname.slice(-4);
+        const check = extensionImageList.includes(extension);
+        if(check){
+            cb(null, true);
+        }else{
+            cb(new Error("extention không hợp lệ"))
+        }
+    }
+})
 
 userRouter.post("/upload-avatar", upload.single('avatar'), (req, res) => {
     res.send("Tính năng upload file")
