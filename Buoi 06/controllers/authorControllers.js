eebbf9b4aa2e368d06b9ca4b82453d72bf814de6
20120230 <duytruong5437@gmail.com>
2022-10-31 17:14:12 +0700

    get all authors

--- Buoi 06/controllers/authorControllers.js
+++ Buoi 06/controllers/authorControllers.js
@@ -9,6 +9,14 @@ const authorController = {
       } catch (error) { 
         res.status(500).json(error);
       }
+    },
+    getAllAuthors: async (req, res) =>{
+        try {
+            const authors = await Author.find();
+            res.status(200).json(authors);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 };
 
