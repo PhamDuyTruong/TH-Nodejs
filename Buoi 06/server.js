26cabb81f01eeaae6eaf2e308bc3d340144ad369
20120230 <duytruong5437@gmail.com>
2022-10-31 16:31:31 +0700

    set up project and model

---
+++ Buoi 06/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+
+dotenv.config()
+
+mongoose.connect((process.env.MONGODB_URL), () => {
+    console.log("Connected to MongoDB");
+})
+
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(cors());
+app.use(morgan("common"));
+
+app.get("/api", (req, res) => {
+
+})
+
+app.listen(8000, () => {
+    console.log("Server is running...");
+})
