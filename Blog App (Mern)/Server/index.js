c93a8e533c416b71cf10b1ef30f3b7c04524bef8
20120230 <duytruong5437@gmail.com>
2022-11-03 23:13:54 +0700

    config backend

---
+++ Blog App (Mern)/Server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'}));
+app.use(cors());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running ${PORT}`);
+})
