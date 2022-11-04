a8582bc78c07371d5639961cdeab23a5549037cc
20120230 <duytruong5437@gmail.com>
2022-11-04 12:42:26 +0700

    get post in backend

--- Blog App (Mern)/Server/index.js
+++ Blog App (Mern)/Server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from 'mongoose'
+import posts from "./routers/post.js";
 
+const URI = 'mongodb+srv://admin:16LbjjPFO2OubAEO@cluster0.gii8yfa.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'}));
 app.use(cors());
 
+app.use("/posts", posts);
 
-app.listen(PORT, () => {
-    console.log(`Server is running ${PORT}`);
-})
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+        console.log(`Server is running ${PORT}`);
+    })
+}).catch(err=> {
+    console.log('Error: ', err)
+});
+
