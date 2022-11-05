9081edc6446100c6716000e8e4838d716b34a22d
20120230 <duytruong5437@gmail.com>
2022-11-05 22:23:13 +0700

    create MVC model

--- Project 07/Backend/index.js
+++ Project 07/Backend/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,8 +12,13 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+        console.log(`Server is running ${PORT}`);
+    })
+}).catch(err=> {
+    console.log('Error: ', err)
+});
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running at ${PORT}`);
-})
+// AUTHENTICATION
