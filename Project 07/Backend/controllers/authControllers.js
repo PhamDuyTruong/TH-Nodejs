bffb006427489e060a915dbe8bbdd9e8d8abd2a4
20120230 <duytruong5437@gmail.com>
2022-11-05 22:39:30 +0700

    create register user

--- Project 07/Backend/controllers/authControllers.js
+++ Project 07/Backend/controllers/authControllers.js
@@ -1,8 +1,25 @@
+const User = require("../models/User")
+const bcrypt = require("bcrypt");
 
 
 const authController = {
     registerUser: async (req, res) => {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hashed = await bcrypt.hash(req.body.password, salt);
 
+            // Create new user
+            const newUser = await new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: hashed
+            });
+            // Save user to database
+            const user = await newUser.save();
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).json({error: error})
+        }
     }
 }
 
--- Project 07/Backend/index.js
+++ Project 07/Backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-
+const authRoute = require("./routers/auth");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/v1/auth", authRoute);
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('Connected to DB');
