d3c17729fd1b594695dd31f14b1ee2fc3f390d54
20120230 <duytruong5437@gmail.com>
2022-11-05 22:52:11 +0700

    login user

--- Project 07/Backend/controllers/authControllers.js
+++ Project 07/Backend/controllers/authControllers.js
@@ -20,6 +20,27 @@ const authController = {
         } catch (error) {
             res.status(500).json({error: error})
         }
+    },
+    loginUser: async (req, res) => {
+        try {
+            const user = await User.findOne({username: req.body.username});
+            if(!user){
+                res.status(404).json("Wrong username !!!");
+            }
+            const validPassword = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+
+            if(!validPassword){
+                res.status(404).json("Wrong password !!!");
+            }
+            if(user && validPassword){
+                res.status(200).json(user);
+            }
+        } catch (error) {
+            res.status(500).json({error: error})
+        }
     }
 }
 
