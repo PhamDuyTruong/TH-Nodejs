14b0f20f02809c30a6165331c82d1f0d5ca3a530
20120230 <duytruong5437@gmail.com>
2022-11-06 16:22:21 +0700

    add config refreshtoken on UI

--- Project 07/Frontend/src/Components/Home/HomePage.jsx
+++ Project 07/Frontend/src/Components/Home/HomePage.jsx
@@ -1,14 +1,18 @@
+import axios from "axios";
 import { useEffect } from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {useNavigate} from 'react-router-dom';
+import jwt_decode from 'jwt-decode'
 import { deleteUser, getAllUsers } from "../../redux/apiRequest";
 import "./home.css";
+import { loginSuccess } from "../../redux/authSlice";
 
 const HomePage = () => {
   const user = useSelector((state) => state.auth.login?.currentUser);
   const userList = useSelector((state) => state.user.users?.allUsers);
   const msg = useSelector((state) => state.user?.msg);
   const dispatch = useDispatch();
+  let axiosJwt = axios.create();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,7 +26,41 @@ const HomePage = () => {
 
   const handleDelete = (id) => {
     deleteUser(user?.accessToken, dispatch, id);
+  };
+
+  const refreshToken = async () => {
+    try {
+      const res = await axios.post("/v1/auth/refresh",{
+        withCredentials: true
+      });
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  axiosJwt.interceptors.request.use(
+    async (config) => {
+      let date = new Date();
+        const decodedToken = jwt_decode(user?.accessToken);
+
+        if(decodedToken.exp < date.getTime()/1000){
+            const data = await refreshToken();
+            const refreshUser = {
+              ...user,
+              accessToken: data.accessToken,
+              refreshToken: data.refreshToken
+            };
+            dispatch(loginSuccess(refreshUser));
+            config.headers["token"] = "Bearer " + data.accessToken;
+        }
+        return config;
+
+    },
+    (err) =>{
+      return Promise.reject(err);
+    }
+  )
 
   return (
     <main className="home-container">
