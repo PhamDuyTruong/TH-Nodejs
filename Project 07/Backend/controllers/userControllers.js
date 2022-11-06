825911355d02df38115a20da2f86ec66ea76faeb
20120230 <duytruong5437@gmail.com>
2022-11-06 07:18:45 +0700

    delete user

--- Project 07/Backend/controllers/userControllers.js
+++ Project 07/Backend/controllers/userControllers.js
@@ -8,6 +8,14 @@ const userController = {
         } catch (error) {
             res.status(500).json(error);
         }
+    },
+    deleteUser: async(req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            res.status(200).json("Delete successfully");
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 };
 
--- Project 07/Backend/routers/user.js
+++ Project 07/Backend/routers/user.js
@@ -5,4 +5,7 @@ const router = require("express").Router();
 // Get ALL user
 router.get("/", userController.getAllUsers);
 
+//Delete user
+router.delete("/:id", userController.deleteUser);
+
 module.exports = router;
