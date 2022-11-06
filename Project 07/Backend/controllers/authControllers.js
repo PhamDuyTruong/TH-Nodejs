7b88f0dbe5964b2aef05cbdaec3f7709eadc4dda
20120230 <duytruong5437@gmail.com>
2022-11-06 09:45:27 +0700

    create new refresh token

--- Project 07/Backend/controllers/authControllers.js
+++ Project 07/Backend/controllers/authControllers.js
@@ -2,6 +2,7 @@ const User = require("../models/User")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+let refreshTokens = [];
 
 const authController = {
     registerUser: async (req, res) => {
@@ -22,6 +23,32 @@ const authController = {
             res.status(500).json({error: error})
         }
     },
+
+    //Generate Token
+    generateAccessToken: (user) => {
+        return jwt.sign({
+            id: user.id,
+            isAdmin: user.isAdmin
+        }, 
+        process.env.MY_SECRETKEY,
+        {
+            expiresIn: "30s"
+        }
+        );
+    },
+    generateRefreshToken: (user) => {
+        return jwt.sign(
+            {
+                id: user.id,
+                isAdmin: user.isAdmin
+            }, 
+            process.env.MY_REFRESHKEY,
+            {
+                expiresIn: "365d"
+            }
+        )
+    },
+    // Login user
     loginUser: async (req, res) => {
         try {
             const user = await User.findOne({username: req.body.username});
@@ -37,22 +64,54 @@ const authController = {
                 res.status(404).json("Wrong password !!!");
             }
             if(user && validPassword){
-                const accessToken = jwt.sign({
-                    id: user.id,
-                    isAdmin: user.isAdmin
-                }, 
-                process.env.MY_SECRETKEY,
-                {
-                    expiresIn: "30d"
-                }
-                );
+                const accessToken = authController.generateAccessToken(user);
+                const refreshToken = authController.generateRefreshToken(user);
+                res.cookie("refreshToken", refreshToken, {
+                    httpOnly: true,
+                    secure:false,
+                    path: "/",
+                    sameSite: "strict",
+                });
+                refreshTokens.push(refreshToken);
                 const {password, ...others} = user._doc;
-                res.status(200).json({...others, accessToken});
+                res.status(200).json({...others, accessToken, refreshToken});
             }
         } catch (error) {
             res.status(500).json({error: error})
         }
+    },
+    requestRefreshToken: async (req, res) => {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) return res.status(401).json("You're not authenticated");
+        if(!refreshTokens.includes(refreshToken)){
+            return res.status(403).json("Refresh token is not valid");
+        }
+        jwt.verify(refreshToken, process.env.MY_REFRESHKEY, (err, user) => {
+            if (err) {
+                console.log(err);
+            }
+            refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+            const newAccessToken = authController.generateAccessToken(user);
+            const newRefreshToken = authController.generateRefreshToken(user);
+            refreshTokens.push(newRefreshToken);
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                secure:false,
+                path: "/",
+                sameSite: "strict",
+            });
+            res.status(200).json({
+                accessToken: newAccessToken,
+                refreshToken: newRefreshToken
+            })
+        })
     }
 }
 
+//STORE Token
+// 1. Localstorage
+// 2. HTTPONLY Cookies
+// 3. REDUX STORE -> ACCESS TOKEN + HTTPONLY COOKIES -> REFRESH TOKEN
+
+// Use BFF Pattern (Backend for Frontend)
 module.exports = authController;
--- Project 07/Backend/routers/auth.js
+++ Project 07/Backend/routers/auth.js
@@ -5,4 +5,6 @@ const router = require("express").Router();
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 
+router.post("/refresh", authController.requestRefreshToken);
+
 module.exports = router;
