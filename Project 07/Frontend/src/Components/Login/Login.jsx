30be6cd054e10e894c1fb102607e8f15db3029d1
20120230 <duytruong5437@gmail.com>
2022-11-06 11:06:02 +0700

    create redux

--- Project 07/Frontend/src/Components/Login/Login.jsx
+++ Project 07/Frontend/src/Components/Login/Login.jsx
@@ -1,14 +1,27 @@
+import React, {useState} from 'react';
 import "./login.css";
 import { Link } from "react-router-dom";
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const newUser = {
+            username: username,
+            password: password
+        };
+        
+    }
+
     return ( 
         <section className="login-container">
             <div className="login-title"> Log in</div>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label>USERNAME</label>
-                <input type="text" placeholder="Enter your username" />
+                <input type="text" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)}/>
                 <label>PASSWORD</label>
-                <input type="password" placeholder="Enter your password" />
+                <input type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}/>
                 <button type="submit"> Continue </button>
             </form>
             <div className="login-register"> Don't have an account yet? </div>
